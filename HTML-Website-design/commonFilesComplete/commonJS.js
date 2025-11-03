b43e811e0456ebda0a1fd0919118ac54281e1c24
ri-kov <rkovalenko1709@gmail.com>
2025-11-02 21:32:00 -0500

    Add files via upload

---
+++ HTML-Website-design/commonFilesComplete/commonJS.js
@@ -0,0 +1,32 @@
+const refreshElements = document.getElementsByClassName('refresh');
+
+function reloadCurrentPage() {
+    window.location.reload(); 
+}
+
+if (refreshElements.length > 0) {
+    refreshElements[0].addEventListener('click', reloadCurrentPage);
+}
+
+function openDropdown() {
+    const dropdown = document.getElementById('dropdownContent');
+    if (dropdown) {
+        dropdown.style.width = '250px';
+    }
+}
+
+function closeDropdown() {
+    const dropdown = document.getElementById('dropdownContent');
+    if (dropdown) {
+        dropdown.style.width = '0';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menu = document.getElementById('menu');
+
+    if (menu) {
+        menu.addEventListener('mouseover', openDropdown);
+        menu.addEventListener('mouseout', closeDropdown);
+    }
+});
