ac6e5b9db9e650e414ddfd072946d6dbe30c9b47
Riza-Asylkhanov <assylkhanovriza@gmail.com>
2025-11-04 21:41:36 -0500

    Add files via upload
    i forgot to change the text from "team page" to "product features" lol

--- HTML-Website-design/Webpages/Product features/javascriptStuff.js
+++ HTML-Website-design/Webpages/Product features/javascriptStuff.js
@@ -1,3 +1,36 @@
+const refreshElements = document.getElementsByClassName('refresh');
+
+function reloadCurrentPage() {
+    window.location.reload(); 
+}
+
+if (refreshElements.length > 0) {
+    refreshElements[0].addEventListener('click', reloadCurrentPage);
+}
+
+function openDropdown() {
+    const dropdown = document.getElementById('dropdownContent');
+    if (dropdown) {
+        dropdown.style.width = '250px';
+    }
+}
+
+function closeDropdown() {
+    const dropdown = document.getElementById('dropdownContent');
+    if (dropdown) {
+        dropdown.style.width = '0';
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menu = document.getElementById('menu');
+
+    if (menu) {
+        menu.addEventListener('mouseover', openDropdown);
+        menu.addEventListener('mouseout', closeDropdown);
+    }
+});
+
 // this makes the title clickable back to menu like how youtube and other websites do it
 const headerTitle = document.getElementById('headerTitle');
 
