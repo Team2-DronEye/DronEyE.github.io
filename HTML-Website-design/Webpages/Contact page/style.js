8eba12e1543bf3df959439b665ad565dfe460bd9
uito13 <itoota1705@gmail.com>
2025-11-11 19:23:51 -0500

    Update style.js

--- HTML-Website-design/Webpages/Contact page/style.js
+++ HTML-Website-design/Webpages/Contact page/style.js
@@ -41,7 +41,7 @@ const translations = {
         "reserved" : "All rights reserved",
         "email-example":"email@example.com",
         "company": "Company Name",
-        "building": "Somewhere Building xF"
+        "building": "Somewhere Building xF",
 
         },
         
