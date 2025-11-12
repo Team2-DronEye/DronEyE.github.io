098f3f6e3bb1f102a1440c0ef8aba85bdfbcd797
ri-kov <rkovalenko1709@gmail.com>
2025-11-11 21:08:59 -0500

    js

--- HTML-Website-design/commonFilesComplete/commonJS.js
+++ HTML-Website-design/commonFilesComplete/commonJS.js
@@ -22,20 +22,17 @@ function closeDropdown() {
     }
 }
 
+
+
 document.addEventListener('DOMContentLoaded', function() {
     const menu = document.getElementById('menu');
-    const dropdown = document.getElementById('dropdownContent');
 
-<<<<<<< HEAD
     if (menu) {
         menu.addEventListener('mouseover', openDropdown);
         menu.addEventListener('mouseout', closeDropdown);
     }
 });
-if (menu && dropdown) {
-=======
-    if (menu && dropdown) {
->>>>>>> cf78dc07bf5f6f0635f046e55861efe3a5551acc
+   if (menu && dropdown) {
         menu.addEventListener('mouseenter', () => {
             dropdown.style.width = '250px';
         });
@@ -56,4 +53,3 @@ if (menu && dropdown) {
             }
         });
     }
-});
