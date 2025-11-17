d6c36b75111d1d16efb0d17cd3dbe3b0258197fb
Nour Elhouda Mohamed <mohamed_nour835@outlook.com>
2025-11-16 23:14:38 -0500

    translation

--- HTML-Website-design/Webpages/Pricing page/PricingPageJavascript.js
+++ HTML-Website-design/Webpages/Pricing page/PricingPageJavascript.js
@@ -77,14 +77,14 @@ const translations = {
         "button_text": "English",
         "contact-us": "Contactez-nous",
         "copyright": "Droit d'auteur© 2025 DronEye. Tous droits réservés.",
-        "Subscriptions": "ABONNEMENT",
+        "Subscriptions": "PLANS D'ABONNEMENT",
         "introText": "Découvrir le monde par un nouveau point de vue avec DRONEYE! Rejoignez la famille de DRONEYE aujourd'hui et trouvez des nouveaux façons d'explorer sans fin! Choisissez ce qui vous intéresse le plus!",
-        "tierOne": 'Le plan Voyageur est recommandé à tous grâce à ses offres exceptionnelles ! <a href="#TravelerTier" class="buyButton"><div style="font-size: 100%; text-align: center;">Touriste</div></a>',
-        "tierTwo": 'Le plan Adventurier offre le meilleur rapport qualité-prix et de nouvelles expériences ! <a href="#AdventurerTier" class="buyButton">Voyageur</a>',
-        "tierThree": 'Le plan Globetrotteur est idéal pour ceux qui souhaitent explorer tous les horizons du monde ! <a href="#GlobetrotterTier" class="buyButton">Globe-Trotteur</a>',
-        "travelerTier": "À 20$ par mois, les membres Voyageur recevront ce qui suit: <div style='margin-left: 40px'><ul><li>Exploration libre des grandes villes à travers le monde</li><li>5 heures de vol par chaque semaine!</li></ul></div>", 
-        "adventurerTier": "À 40$ par mois, les membres Aventurier recevront ce qui suit: <div style='margin-left: 40px'><ul><li>Tous les avantages d'Étage Touriste</li><li>Exploration libre de tous les villes et villages à travers le monde</li><li>Personnalisation de votre drone</li><li>15 heures de vol par chaque semaine!</li></ul></div>",
-        "globetrotterTier": "À 70$ par mois, les membres Globetrotteur recevront ce qui suit: <div style='margin-left: 40px'><ul><li>Tous les avantages des precedents Étages</li><li>Exploration entiérement libre</li><li>Rien de limite sur des heures de vol</li><li>Priorité absolue sur le choix du mod<le de drone</li></ul></div>"
+        "tierOne": 'Le plan Voyageur est recommandé à tous grâce à ses offres exceptionnelles ! <a href="#TravelerTier" class="buyButton"><div style="font-size: 100%; text-align: center;">Voyageur</div></a>',
+        "tierTwo": 'Le plan Adventurier offre le meilleur rapport qualité-prix et de nouvelles expériences ! <a href="#AdventurerTier" class="buyButton">Adventurier</a>',
+        "tierThree": 'Le plan Globetrotteur est idéal pour ceux qui souhaitent explorer tous les horizons du monde ! <a href="#GlobetrotterTier" class="buyButton">Globetrotteur</a>',
+        "travelerTier": "À 20$ par mois, les membres du plan Voyageur recevront ce qui suit: <div style='margin-left: 40px'><ul><li>Exploration libre des grandes villes à travers le monde</li><li>5 heures de vol par chaque semaine!</li></ul></div>", 
+        "adventurerTier": "À 40$ par mois, les membres du plan Aventurier recevront ce qui suit: <div style='margin-left: 40px'><ul><li>Tous les avantages du plan Voyageur</li><li>Exploration libre de tous les villes et villages à travers le monde</li><li>Personnalisation de votre drone</li><li>15 heures de vol par chaque semaine!</li></ul></div>",
+        "globetrotterTier": "À 70$ par mois, les membres du plan Globetrotteur recevront ce qui suit: <div style='margin-left: 40px'><ul><li>Tous les avantages des precedents Étages</li><li>Exploration entiérement libre</li><li>Rien de limite sur des heures de vol</li><li>Priorité absolue sur le choix du mod<le de drone</li></ul></div>"
     }
 };
 document.addEventListener('DOMContentLoaded', function() {
